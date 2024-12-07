eb89a12d2949457c2c90aea493ecc6a0be9dbe7a
shahramsamar <shahramsamar2010@gmail.com>
2024-12-07 23:05:48 +0330

    Update urls.py
    use pattern_name

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,14 @@ from blog import views
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
+app_name ='blog'
+
 urlpatterns = [
     # path('',TemplateView.as_view(template_name='index.html', extra_context={'name': 'shahram'})),
     path('', views.IndexView.as_view(), name='cbv-index'),
     path('go-to-django', RedirectView.as_view(url='https://www.djangoproject.com/'),name='redirect-to-django'),
+    path('go-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'),name='redirect-to-index'),
+
+    
 
 ]
