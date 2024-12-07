18f83389b0c10ef815b5dd6630548b52a9bfc31e
shahramsamar <shahramsamar2010@gmail.com>
2024-12-07 17:18:11 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,11 @@ from django.views.generic.base import TemplateView
 
 
 ''' just uses for get request and  not request for form and action like delete ,edit,and more'''
+
+
 class IndexView(TemplateView):
     template_name = 'index.html'
-    
-    
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = 'shahram'
