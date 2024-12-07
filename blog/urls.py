a6ad76866d9c20dfa40922da1690d11a25e6f54f
shahramsamar <shahramsamar2010@gmail.com>
2024-12-07 17:36:54 +0330

    added redirect view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from blog import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # path('',TemplateView.as_view(template_name='index.html', extra_context={'name': 'shahram'})),
     path('', views.IndexView.as_view(), name='cbv-index'),
+    path('go-to-django', RedirectView.as_view(url='https://www.djangoproject.com/'),name='redirect-to-django'),
 
 ]
