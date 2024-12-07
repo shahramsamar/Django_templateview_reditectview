0eef5310639049400143906d2bee809e2283b5c7
shahramsamar <shahramsamar2010@gmail.com>
2024-12-07 17:14:48 +0330

    use class for templateview

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from blog import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='index.html', extra_context={'name': 'shahram'})),
+    # path('',TemplateView.as_view(template_name='index.html', extra_context={'name': 'shahram'})),
+    path('', views.IndexView.as_view(), name='cbv-index'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+''' just uses for get request and  not request for form and action like delete ,edit,and more'''
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'shahram'
+        # context['post'] = Post.Object.all()
+        return context
