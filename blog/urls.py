209ad8e62ebe749e77634d65a01f7cc30dd6b8be
shahramsamar <shahramsamar2010@gmail.com>
2024-12-07 23:29:13 +0330

    use class redirect view

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,10 @@ app_name ='blog'
 urlpatterns = [
     # path('',TemplateView.as_view(template_name='index.html', extra_context={'name': 'shahram'})),
     path('', views.IndexView.as_view(), name='cbv-index'),
-    path('go-to-django', RedirectView.as_view(url='https://www.djangoproject.com/'),name='redirect-to-django'),
-    path('go-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'),name='redirect-to-index'),
+    path('go-to-django', RedirectView.as_view(url='https://www.djangoproject.com/'), name='redirect-to-django'),
+    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='redirect-to-index'),
+    path('go-to-djangoo', views.RedirectToDjango.as_view(), name='redirect-to-django'),
+
 
     
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView, RedirectView
 
 
 ''' just uses for get request and  not request for form and action like delete ,edit,and more'''
@@ -13,3 +13,12 @@ class IndexView(TemplateView):
         context['name'] = 'shahram'
         # context['post'] = Post.Object.all()
         return context
+
+''' just redirects to '''
+class RedirectToDjango(RedirectView):
+    url = 'https://www.djangoproject.com/'
+    
+    
+    def get_redirect_url(self, *args, **kwargs):
+        # post = get_object_or_404(*Post,pk=kwargs['pk'])
+        return super().get_redirect_url(*args, **kwargs)
